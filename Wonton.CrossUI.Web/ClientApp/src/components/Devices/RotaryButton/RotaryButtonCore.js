e9d337dda579cc2ad464bca2c81e0f99668bba45
Hi2129 <2522717938@qq.com>
2021-09-11 20:32:59 +0800

    优化了：RotaryButton的外观

--- Wonton.CrossUI.Web/ClientApp/src/components/Devices/RotaryButton/RotaryButtonCore.js
+++ Wonton.CrossUI.Web/ClientApp/src/components/Devices/RotaryButton/RotaryButtonCore.js
@@ -13,7 +13,7 @@ export class RotaryButtonCore extends Component {
     render() {
         return (
             <div>
-                <Button id="display" color="primary" disabled>{this.props.value}</Button>
+                <Button id="display" disabled>{this.props.value}</Button>
                 <Button id="button1" onClick={() => { this.props.onCheckClick("+") }}>+</Button>
                 <Button id="button2" onClick={() => { this.props.onCheckClick("-") }}>-</Button>
             </div>
